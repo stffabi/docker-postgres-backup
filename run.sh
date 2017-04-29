267374928c3e83799a7de8a80c74501691049ed0
Johan Swetzén <johan.swetzen@cybercom.com>
2017-04-29 04:51:27 +0200

    Fix some spelling

--- run.sh
+++ run.sh
@@ -70,13 +70,13 @@ touch /mysql_backup.log
 tail -F /mysql_backup.log &
 
 if [ -n "${INIT_BACKUP}" ]; then
-    echo "=> Create a backup on the startup"
+    echo "=> Create a backup on startup"
     /backup.sh
 elif [ -n "${INIT_RESTORE_LATEST}" ]; then
-    echo "=> Restore lates backup"
+    echo "=> Restore latest backup"
     until nc -z $MYSQL_HOST $MYSQL_PORT
     do
-        echo "waiting database container..."
+        echo "waiting for database container..."
         sleep 1
     done
     ls -d -1 /backup/* | tail -1 | xargs /restore.sh
